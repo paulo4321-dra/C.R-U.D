54686e6685fa21e259fd7772553ec52b0853e2a3
PAULO FRANCA <paulo.franca@senacsp.edu.br>
2025-04-03 17:20:57 -0300

    C.R.U.D

---
+++ cadastrodeclientes/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace cadastrodeclientes
+{
+    public partial class frmCadastrodeClientes : Form
+    {
+        public frmCadastrodeClientes()
+        {
+            InitializeComponent();
+        }
+        private bool IsValidEmail(string email)
+        {
+            string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            Regex regex = new Regex(pattern);
+            return regex.IsMatch(email);
+        }
+        private bool IsValidCPFLegth(string cpf)
+        {
+            cpf = cpf.Replace(".", "").Replace("-", "");
+
+            if (cpf.Length !=  11 || !cpf.All(char.IsDigit))
+            {
+                return false;
+            }
+            return true;
+        }
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Validação de campos obrigatórios 
+
+                if(string.IsNullOrEmpty(txtNomeCompleto.Text.Trim()) ||
+                   string.IsNullOrEmpty(txtEmail.Text.Trim()) ||
+                   string.IsNullOrEmpty(txtCPF.Text.Trim()))
+                {
+                    MessageBox.Show("Todos os campos devem ser preenchidos.",
+                                    "Validação",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Warning);
+                    return; // impede o prosseguimento se algum campo estiver vazio
+                }
+                // Validação do E-mail
+
+                string email = txtEmail.Text.Trim();
+                if (!IsValidEmail(email))
+                {
+                    MessageBox.Show("E-mail inválido. Certifique-se de que o E-mail está no formato correto.",
+                                    "Validação",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Validação do CPF
+                string cpf = txtCPF.Text.Trim();
+                if (!IsValidCPFLegth(cpf))
+                {
+                    MessageBox.Show("CPF inválido. Certifique-se de que o CPF tenha 11 digitos númericos",
+                                    "Validação",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                //Trata outros tipos de erro
+
+                MessageBox.Show("Ocorreu: " + ex.Message,
+                    "Erro",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+             }
+        }
+    }
+}
